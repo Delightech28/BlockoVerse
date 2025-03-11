ca98db6ada4782619bbb6eb3a1da9728b33fb09b
Delightech28 <desolomon07@gmail.com>
2025-03-11 19:56:52 +0100

    Allow Render host in Vite

--- vite.config.js
+++ vite.config.js
@@ -1,7 +1,11 @@
 import { defineConfig } from 'vite';
+
 export default defineConfig({
   server: {
-    port: process.env.PORT || 4173, // Use Render’s PORT or default to 4173
-    host: "0.0.0.0"
+    host: true, // Allows external access
+    strictPort: true,
+  },
+  preview: {
+    allowedHosts: ['blockoverse.onrender.com'] // Add your Render domain here
   }
 });

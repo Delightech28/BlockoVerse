c2dddb92f437a2e93abe92bb8aa64f0ec3ccc14d
Delightech28 <desolomon07@gmail.com>
2025-03-11 19:42:40 +0100

    Fix: Set port for Render deployment

--- vite.config.js
+++ vite.config.js
@@ -1,7 +1,6 @@
-import { defineConfig } from 'vite'
-import react from '@vitejs/plugin-react'
-
-// https://vite.dev/config/
 export default defineConfig({
-  plugins: [react()],
-})
+  server: {
+    port: process.env.PORT || 4173, // Use Render’s PORT or default to 4173
+    host: "0.0.0.0"
+  }
+});

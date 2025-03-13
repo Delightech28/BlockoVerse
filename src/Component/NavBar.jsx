4191591696f63187fe8f4ea56e3b9a7d25368ebf
Delight Tech <desolomon07@gmail.com>
2025-03-13 17:34:36 +0100

    Update NavBar.jsx

--- src/Component/NavBar.jsx
+++ src/Component/NavBar.jsx
@@ -1,14 +1,26 @@
-
-import React, { useState } from "react";
+import React, { useState, useEffect } from "react";
 import { Link } from "react-router-dom";
-//import "bootstrap-icons/font/bootstrap-icons.css"; // Bootstrap Icons
 import "./NavBar.css";
 
 const NavBar = () => {
   const [isOpen, setIsOpen] = useState(false);
+  const [email, setEmail] = useState(localStorage.getItem("userEmail") || "");
+
+  useEffect(() => {
+    const handleStorageChange = () => {
+      setEmail(localStorage.getItem("userEmail") || "");
+    };
+
+    window.addEventListener("storage", handleStorageChange);
+    return () => window.removeEventListener("storage", handleStorageChange);
+  }, []);
 
   const toggleMenu = () => {
     setIsOpen(!isOpen);
+  };
+
+  const closeMenu = () => {
+    setIsOpen(false);
   };
 
   return (
@@ -17,35 +29,27 @@ const NavBar = () => {
         <Link className="navbar-brand" to="/">
           Blockoverse
         </Link>
-        <button
-          className="navbar-toggler"
-          type="button"
-          onClick={toggleMenu}
-        >
-          <i
-            className={`bi ${isOpen ? "bi-x" : "bi-list"}`}
-            style={{
-              fontSize: "36px", /* Bigger icon */
-            }}
-          ></i>
+        <button className="navbar-toggler" type="button" onClick={toggleMenu}>
+          <i className={`bi ${isOpen ? "bi-x" : "bi-list"}`} style={{ fontSize: "36px" }}></i>
         </button>
         <div className={`collapse navbar-collapse ${isOpen ? "show" : ""}`}>
           <ul className="navbar-nav ms-auto">
             <li className="nav-item">
-              <Link className="nav-link" to="/home">Home</Link>
+              <Link className="nav-link" to="/home" onClick={closeMenu}>Home</Link>
             </li>
             <li className="nav-item">
-              <Link className="nav-link" to="/tasks">Tasks</Link>
+              <Link className="nav-link" to="/tasks" onClick={closeMenu}>Tasks</Link>
             </li>
             <li className="nav-item">
-              <Link className="nav-link" to="/rewards">Rewards</Link>
+              <Link className="nav-link" to="/rewards" onClick={closeMenu}>Rewards</Link>
             </li>
             <li className="nav-item">
-              <Link className="nav-link" to="/referral">Referral</Link>
+              <Link className="nav-link" to="/referral" onClick={closeMenu}>Referral</Link>
             </li>
             <li className="nav-item">
-              <Link className="nav-link" to="/nft">NFT</Link>
+              <Link className="nav-link" to="/nft" onClick={closeMenu}>NFT</Link>
             </li>
+            
           </ul>
         </div>
       </div>

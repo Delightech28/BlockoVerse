b817fe4617c005787ab10df97e4fd8926f8cf406
Delight Tech <desolomon07@gmail.com>
2025-03-13 18:23:36 +0100

    Update App.jsx

--- src/App.jsx
+++ src/App.jsx
@@ -2,6 +2,7 @@ import { BrowserRouter as Router, Routes, Route, Navigate } from "react-router-d
 import WaitlistForm from "./Component/WaitlistForm";
 import VerifyCode from "./Component/VerifyCode";
 import Dashboard from "./Component/Dashboard";
+import Profile from "./Component/Profile";  // Import Profile component
 import "bootstrap/dist/css/bootstrap.min.css";
 import "bootstrap/dist/js/bootstrap.bundle.min.js";
 
@@ -13,7 +14,7 @@ function App() {
         <Route path="/register" element={<WaitlistForm />} />
         <Route path="/verify" element={<VerifyCode />} />
         <Route path="/dashboard" element={<Dashboard />} />
-      
+        <Route path="/profile" element={<Profile />} />  {/* Add Profile Route */}
         <Route path="/signup" element={<WaitlistForm />} />
       </Routes>
     </Router>
